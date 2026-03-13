82d0cdfc7a446382413d72997121660120a5a217
Christoph Maier <christoph.maier@ieee.org>
2026-03-13 14:35:43 +0100

    sudelbuecher/Claude/dacbuffer_klayout.py output.gds : parsed python script hand edited

---
+++ sudelbuecher/Claude/dacbuffer_klayout.py
@@ -0,0 +1,73 @@
+import pya
+
+layout = pya.Layout()
+layout.dbu = 0.001
+
+# --- cell declarations ---
+cell_dacbuffer = layout.create_cell("dacbuffer")
+
+# === dacbuffer ===
+_path = pya.DPath([pya.DPoint(0, 0.25), pya.DPoint(37.8, 0.25)], 0.5)
+cell_dacbuffer.shapes(layout.layer(8, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 13.095), pya.DPoint(37.8, 13.095)], 0.7)
+cell_dacbuffer.shapes(layout.layer(8, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 25.94), pya.DPoint(37.8, 25.94)], 0.5)
+cell_dacbuffer.shapes(layout.layer(8, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 0.865), pya.DPoint(37.8, 0.865)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 3.295), pya.DPoint(37.8, 3.295)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 6.525), pya.DPoint(37.8, 6.525)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 19.665), pya.DPoint(37.8, 19.665)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 22.895), pya.DPoint(37.8, 22.895)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+_path = pya.DPath([pya.DPoint(0, 25.325), pya.DPoint(37.8, 25.325)], 1.73)
+cell_dacbuffer.shapes(layout.layer(30, 0)).insert(_path)
+cell_dacbuffer.shapes(layout.layer(189, 4)).insert(
+    pya.DPolygon([pya.DPoint(0, -0.07), pya.DPoint(0, 26.26), pya.DPoint(37.8, 26.26), pya.DPoint(37.8, -0.07), pya.DPoint(0, -0.07)]))
+cell_dacbuffer.shapes(layout.layer(8, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 12.745), pya.DPoint(0, 13.445), pya.DPoint(37.8, 13.445), pya.DPoint(37.8, 12.745), pya.DPoint(0, 12.745)]))
+cell_dacbuffer.shapes(layout.layer(8, 25)).insert(
+    pya.DText("Iout",
+              pya.DTrans(0, False, pya.DVector(18.9, 13.095))))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 0), pya.DPoint(0, 1.73), pya.DPoint(37.8, 1.73), pya.DPoint(37.8, 0), pya.DPoint(0, 0)]))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 2.43), pya.DPoint(0, 4.16), pya.DPoint(37.8, 4.16), pya.DPoint(37.8, 2.43), pya.DPoint(0, 2.43)]))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 5.66), pya.DPoint(0, 7.39), pya.DPoint(37.8, 7.39), pya.DPoint(37.8, 5.66), pya.DPoint(0, 5.66)]))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 18.8), pya.DPoint(0, 20.53), pya.DPoint(37.8, 20.53), pya.DPoint(37.8, 18.8), pya.DPoint(0, 18.8)]))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 22.03), pya.DPoint(0, 23.76), pya.DPoint(37.8, 23.76), pya.DPoint(37.8, 22.03), pya.DPoint(0, 22.03)]))
+cell_dacbuffer.shapes(layout.layer(30, 2)).insert(
+    pya.DPolygon([pya.DPoint(0, 24.46), pya.DPoint(0, 26.19), pya.DPoint(37.8, 26.19), pya.DPoint(37.8, 24.46), pya.DPoint(0, 24.46)]))
+cell_dacbuffer.shapes(layout.layer(8, 25)).insert(
+    pya.DText("VSS",
+              pya.DTrans(0, False, pya.DVector(18.9, 0.25))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VSS",
+              pya.DTrans(0, False, pya.DVector(18.9, 0.865))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VDD",
+              pya.DTrans(0, False, pya.DVector(18.9, 3.295))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VDD",
+              pya.DTrans(0, False, pya.DVector(18.9, 6.525))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VDD",
+              pya.DTrans(0, False, pya.DVector(18.9, 19.665))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VDD",
+              pya.DTrans(0, False, pya.DVector(18.9, 22.895))))
+cell_dacbuffer.shapes(layout.layer(30, 25)).insert(
+    pya.DText("VSS",
+              pya.DTrans(0, False, pya.DVector(18.9, 25.325))))
+cell_dacbuffer.shapes(layout.layer(8, 25)).insert(
+    pya.DText("VSS",
+              pya.DTrans(0, False, pya.DVector(18.9, 25.94))))
+
+# Save
+layout.write("output.gds")
