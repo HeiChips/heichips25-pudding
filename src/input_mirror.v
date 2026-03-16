663e5f401df06bdc3c55e5b0144153c2b8a9f3e2
Christoph Maier <christoph.maier@ieee.org>
2026-03-16 13:12:28 +0100

    librelane/config.yaml src/dac4x32module.v input_mirror.v : verilog for dac4x32module synthesis

---
+++ src/input_mirror.v
@@ -0,0 +1,25 @@
+// File: input_mirror.v
+// input current mirror (etc.) digital placeholder
+// Yosys hints: blackbox + keep_hierarchy.
+(* blackbox *)
+module input_mirror (
+    inout       Iout,
+    inout [7:0] VbiasP,
+    inout i_in,
+    inout i_out,
+    inout [3:0] VDDA
+);
+
+//  wire bias;
+//  wire casc;
+//  wire vdda;
+
+//  assign VbiasP[0] = bias;
+//  assign VbiasP[1] = bias;
+//  assign VcascP[0] = casc;
+//  assign VcascP[1] = casc;
+//  assign VDDA[3] = vdda;
+//  assign VDDA[2] = vdda;
+//  assign VDDA[1] = vdda;
+//  assign VDDA[0] = vdda;
+endmodule
